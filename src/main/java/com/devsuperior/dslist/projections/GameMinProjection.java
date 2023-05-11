34b5309242db25c881329d4d27feadbb462afa26
Mercio franca <mercioalbuquerque@hotmail.com>
2023-05-11 03:50:23 -0300

    SQL - Projections

---
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslist.projections;
+
+public interface GameMinProjection{
+
+	Long getId();
+	String GetTitle();
+	Integer getYear();
+	String getImgUrl();
+	String getShortDescription();
+	Integer getPosition();
+	String getTitle();	
+}
